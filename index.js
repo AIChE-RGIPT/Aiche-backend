02f3e6a558482e0049d70c52d7f13f44c7431255
AIChE-RGIPT <aiche@rgipt.ac.in>
2024-08-18 02:12:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,24 +10,18 @@ const buyRouter = require('./routes/buy.js');
 let port = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: 'https://aichergipt.com',
+  origin: 'https://www.aichergipt.com',  // Corrected to match your frontend URL
   methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
   credentials: true
 };
 
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://www.aichergipt.com");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
-
+// Use CORS middleware with the defined options
 app.use(cors(corsOptions));
+
 app.use(express.json()); 
 app.use("/", contactRouter);
 app.use("/", registrationRouter);
 app.use("/", buyRouter);
-
 
 app.listen(port, () => {
     console.log("Server is listening at the port", port);
